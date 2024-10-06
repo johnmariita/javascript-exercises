da12e52e9d2273e47a391caf47622ec64a4b4ebc
johnmariita <johnmariita421@gmail.com>
2024-10-06 06:03:33 +0300

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    strArray = str.split("");
+    strArray.reverse();
+    str = strArray.join('');
+    return str;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
