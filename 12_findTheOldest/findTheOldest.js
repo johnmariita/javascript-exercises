fe83bf54607be94ff4a16090c4768af7c85a61f5
johnmariita <johnmariita421@gmail.com>
2024-10-06 11:37:41 +0300

    find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((acc, curr) => {
+        const d = new Date();
+        const currAge = curr.yearOfDeath ? 
+            curr.yearOfDeath - curr.yearOfBirth : 
+            d.getFullYear() - curr.yearOfBirth;
+        const accAge = acc.yearOfDeath ? 
+            acc.yearOfDeath - acc.yearOfBirth : 
+            d.getFullYear() - acc.yearOfBirth;
+        return currAge > accAge ? curr : acc;
+    });
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
