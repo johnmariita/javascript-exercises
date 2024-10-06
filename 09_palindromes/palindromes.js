27bf38b9b974c9cb1cb3266f413bb1088b9e7e45
johnmariita <johnmariita421@gmail.com>
2024-10-06 09:14:23 +0300

    check if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    if (str[0] !== str[str.length - 1]) return false;
+    if (str.length === 1 || str.length === 0) return true;
+    return palindromes(str.substring(1, str.length - 1));
 };
-
 // Do not edit below this line
 module.exports = palindromes;
