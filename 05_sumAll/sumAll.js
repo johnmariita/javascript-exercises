8af85e1faa63aa0171620b10fa0cae3c87b6c9a6
johnmariita <johnmariita421@gmail.com>
2024-10-06 06:57:39 +0300

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) return "ERROR";
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = start; i >= end; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
+console.log(sumAll(2.5, 10));
 // Do not edit below this line
 module.exports = sumAll;
