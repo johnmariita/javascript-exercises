7fd89b8531cfbec24d3af7a1b5f572909843d4c3
johnmariita <johnmariita421@gmail.com>
2024-10-06 09:32:19 +0300

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS";
+    if (n == 0 || n == 1) return n;
+    return fibonacci(n - 2) + fibonacci(n - 1);
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
