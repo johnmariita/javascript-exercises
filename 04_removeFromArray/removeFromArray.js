19a587e542cbffba32cc76c495616ec21631e7c8
johnmariita <johnmariita421@gmail.com>
2024-10-06 06:41:22 +0300

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        while (array.includes(args[i])) {
+            array.forEach((item, index) => {
+                if (item === args[i]) {
+                    array.splice(index, 1);
+                }
+            });
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
