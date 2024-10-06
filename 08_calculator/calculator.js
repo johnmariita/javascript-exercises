e7d69626947332136819533762a1b559d3b25214
johnmariita <johnmariita421@gmail.com>
2024-10-06 08:04:47 +0300

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, curr) => acc += curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc *= curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fact = 1;
+  for (let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
